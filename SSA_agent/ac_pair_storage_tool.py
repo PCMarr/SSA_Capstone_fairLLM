403e8fdf5e342f57946f436b076c209d3a92fa63
griffingreenwood21 <141801624+griffingreenwood21@users.noreply.github.com>
2026-02-13 10:29:00 -0700

    Added sqlite3 package to ac_pair_storage_tool.py but don't have OpenAI API key to test yet

--- SSA_agent/ac_pair_storage_tool.py
+++ SSA_agent/ac_pair_storage_tool.py
@@ -1,5 +1,5 @@
 from fairlib.core.interfaces.tools import AbstractTool
-import json
+import sqlite3
 
 class PairStorageTool(AbstractTool):
     name = "pair_storage_tool"
@@ -13,7 +13,7 @@ class PairStorageTool(AbstractTool):
 
     def use(self, expression: str) -> str:
         """
-        Takes actor concept tuple, adds it to a growing JSON file
+        Takes actor concept tuple, adds it to a growing database
         """
         concept_list = ["space as a common resource", "governing orbital sustainability", "profit", "international cooperation", "science and innovation", "national economic gains", "societal development", "diplomatic tool"]
         actor_list =  ["United States","China","Russia","France","UK","NATO", "United Nations","SpaceX"]
@@ -27,24 +27,36 @@ class PairStorageTool(AbstractTool):
         if(pair[1] not in concept_list):
             raise Exception(f"{pair[1]} is not one of the accepted concepts")
         
-        with open("/home/peter-marriott/SSA_Capstone_fairLLM/SSA_agent/actor_concept.json", "r") as file:
-            json_str = file.read()
-            json_obj = json.loads(json_str)
+        #put actor concept pairs into database https://docs.python.org/3/library/sqlite3.html
+        connection = sqlite3.connect("actor_concept.db")
+        cursor = connection.cursor
 
-        # adds actor if not already in database
-        if(json_obj.get(pair[0]) is None):
-            json_obj[pair[0]] = dict()
+        cursor.execute("CREATE TABLE pairs(actor, concept, timestamp)")
+
+        cursor.execute("""
+                       INSERT INTO pairs VALUES
+                       (pair[0], pair[1], 02-13-26))
+                       """)
+
+
+        # with open("/home/peter-marriott/SSA_Capstone_fairLLM/SSA_agent/actor_concept.json", "r") as file:
+        #     json_str = file.read()
+        #     json_obj = json.loads(json_str)
+
+        # # adds actor if not already in database
+        # if(json_obj.get(pair[0]) is None):
+        #     json_obj[pair[0]] = dict()
         
-        # adds concept if not already in database
-        if(json_obj[pair[0]].get(pair[1]) is None):
-            json_obj[pair[0]][pair[1]] = 1
-        else:
-            json_obj[pair[0]][pair[1]] += 1
+        # # adds concept if not already in database
+        # if(json_obj[pair[0]].get(pair[1]) is None):
+        #     json_obj[pair[0]][pair[1]] = 1
+        # else:
+        #     json_obj[pair[0]][pair[1]] += 1
 
-        with open("/home/peter-marriott/SSA_Capstone_fairLLM/SSA_agent/actor_concept.json", "w") as file:
-            json_str = json.dumps(json_obj)
-            file.write(json_str)
-        return(f"Successfully stored ({pair[0]}, {pair[1]})")
+        # with open("/home/peter-marriott/SSA_Capstone_fairLLM/SSA_agent/actor_concept.json", "w") as file:
+        #     json_str = json.dumps(json_obj)
+        #     file.write(json_str)
+        # return(f"Successfully stored ({pair[0]}, {pair[1]})")
     
 if __name__ == "__main__":
     tool = PairStorageTool()
